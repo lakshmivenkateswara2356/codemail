3d48c9453ac0c28e1a233d769eceb225e7dd5c27
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-18 16:42:52 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import ReactFlow, { addEdge, MiniMap, Controls, Background } from 'react-flow-renderer';
+
+const initialElements = [];
+
+const Flowchart = () => {
+  const [elements, setElements] = useState(initialElements);
+
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
+
+  return (
+    <ReactFlow
+      elements={elements}
+      onElementsRemove={onElementsRemove}
+      onConnect={onConnect}
+      deleteKeyCode={46}
+      style={{ width: '100%', height: '100vh' }}
+    >
+      <MiniMap />
+      <Controls />
+      <Background />
+      <h1>hello</h1>
+    </ReactFlow>
+  );
+};
+
+export default Flowchart;
